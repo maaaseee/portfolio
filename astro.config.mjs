0973c4a2cfff0c32d3281bc8af2db86a30cc8c7a
Aieta Federico <maxe1111@hotmail.com>
2026-05-09 21:04:54 -0300

    Pantalla de carga + seccion Hero mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,25 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      }
+    ],
+  },
+});
